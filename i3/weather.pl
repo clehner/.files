95900141f88680e2109c75ebec8e6925ca991673
Charles Lehner <celehner1@gmail.com>
2013-12-16 12:25:00 -0500

    Shorten weather.pl

--- i3/weather.pl
+++ i3/weather.pl
@@ -4,7 +4,7 @@ use strict;
 use HTTP::Request;
 use LWP::UserAgent;
 
-my $city_code = $ARGV[0];
+my $city_code = $ARGV[0] // 'USNY1232';
 my $days = +$ARGV[1] // 4;
 my $interval = ($ARGV[2] // 30) * 60;
 
@@ -12,12 +12,15 @@ my $query = 'select item from weather.forecast where location="' . $city_code . 
 my $request = HTTP::Request->new(GET =>
 	'http://query.yahooapis.com/v1/public/yql?q=' . $query . '&format=json');
 
-my @icons = qw(☀ ☁ ☂ ☃ ?);
-my %codes;
-$codes{$_} = 0 for qw(28 32 33 34 36);
-$codes{$_} = 1 for qw(19 20 21 22 23 24 25 26 27 28 29 30 44);
-$codes{$_} = 2 for qw(0 1 2 3 4 5 6 9 11 12 37 38 39 40 45 47);
-$codes{$_} = 3 for qw(7 8 10 13 14 15 16 17 18 35 41 42 43 46);
+#my @icons = qw(☀ ☁ ☂ ☃ ☔ ⛈ ⛇ ?⛄ );
+my @icons;
+$icons[$_] = '☀' for qw(28 32 33 34 36); # sun
+$icons[$_] = '☁' for qw(19 20 21 22 23 24 25 26 27 28); # cloud
+$icons[$_] = '⛅' for qw(29 30 30 44 29); # cloud cover
+$icons[$_] = '☂' for qw(5 6 9 11 12); # rain
+$icons[$_] = '☔' for qw(37 38 39 40 45 47); # more rain
+$icons[$_] = '⛈' for qw(0 1 2 3 4 ); # storm
+$icons[$_] = '❄' for qw(7 8 10 13 14 15 16 17 18 35 41 42 43 46); # snow ☃⛇
 
 # Don't buffer output
 $| = 1;
@@ -28,32 +31,8 @@ sub get_icon {
 	my $code = shift;
 	my $text = lc shift;
 
-	# identify by code
-	if (exists $codes{$code}) {
-		$code = $codes{$code};
-
-	# sun
-	} elsif ($text =~ /sun/) {
-		$code = 0;
-
-	# cloud / early rain
-	} elsif ($text =~ /cloud/) {
-		$code = 1;
-
-	# rain
-	} elsif ($text =~ /rain/) {
-		$code = 2;
-
-	# snow
-	} elsif ($text =~ /snow/) {
-		$code = 3;
-
-	# dunno
-	} else {
-		$code = -1;
-	}
-
-	return $icons[$code];
+	# icon identify by code
+	return $icons[$code] // '?';
 }
 
 sub get_forecast {
